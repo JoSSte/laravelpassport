d71cec0a35af0a56f28a452e5ec64d09966e489d
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-10-11 20:47:38 +0200

    added proper scopes and expiry on tokens

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,13 +29,18 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes();
 
         Passport::tokensCan([
-            'place-orders' => 'Place orders',
-            'check-status' => 'Check order status',
+            'users:read' => 'Get userlist',
+            'users:create' => 'Create user',
         ]);
 
         Passport::setDefaultScope([
-            'check-status',
-            'place-orders',
+            'users:read',
         ]);
+
+        Passport::tokensExpireIn(now()->addDays(15));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+    
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
